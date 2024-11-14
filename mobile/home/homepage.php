8a075cf40ab5b2bf0aadf295085ccff8f4e719a1
mohammed mojeed <mohammedmojeed3@gmail.com>
2024-11-14 02:49:22 -0800

    all things are corrected  waiting for logo update

---
+++ mobile/home/homepage.php
@@ -0,0 +1,18 @@
+<?php 
+
+    /*
+     
+      
+        Website: www.topupmate.com
+        
+        PWhatsapp: 07032529431
+    */
+
+    
+    $design = $data3->homedesign;
+    $color = $data3->sitecolor;
+    $name = $data3->sitename;
+
+    include("homepages/homepage".$design.".php");
+
+?>
